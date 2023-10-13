a580ed7b2a9c8144dcfa020ef37249df680d7b8b
Von Coleman <von.coleman@yahoo.com>
2023-10-12 21:29:27 -0500

    first commit

---
+++ src/Pemdas.java
@@ -0,0 +1,8 @@
+public class Pemdas { //I created a public class named Pemdas. I did that here!
+    public static void main(String[] args){     //My Main Man. The beginning of the program. The start.
+        System.out.println( -5 + 8 *6); //Looks like I can create multiple lines using System.out.println
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3*2 - 8%3);
+    }
+}
